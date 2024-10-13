63706b010c6f369a1354c4cfe718d622553ff16a
malenceamat <yatikivan@gmail.com>
2024-10-13 20:12:17 +0300

    123

--- resources/views/content/general_content/social.blade.php
+++ resources/views/content/general_content/social.blade.php
@@ -1,12 +1,7 @@
 <section class="well3 bg-primary  center">
     <div class="container">
         <ul class="inline-list">
-            <li><a class="fa fa-facebook" href="index.html#">facebook</a></li>
-            <li><a class="fa fa-twitter" href="index.html#">twitter</a></li>
-            <li><a class="fa fa-google-plus" href="index.html#">google-plus</a></li>
-            <li><a class="fa fa-instagram" href="index.html#">instagram</a></li>
-            <li><a class="fa fa-skype" href="index.html#">skype</a></li>
-            <li><a class="fa fa-vimeo-square" href="index.html#">vimeo</a></li>
+            <li><a href="https://vk.com/vremyachistotisurgut86"><i class="bi bi-messenger" style="color: white;font-size: 15px"> В контакте</i></a></li>
         </ul>
     </div>
 </section>
